dd17404d2ed8665c9212f0b278e10dffa73582b6
sean linke <sean.tnl@gmail.com>
2019-07-15 22:08:17 -0700

    update routes to toggle dependent on logged in

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
 import Lobby from './Lobby';
 import Login from './Login';
@@ -7,15 +8,35 @@ import Register from './Register';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/" exact component={Lobby}/>
-      <Route path="/login" exact component={Login}/>
-      <Route path="/register" exact component={Register}/>
-      <Route path="/room/:id" component={PokerRoom}/>
-    </div>
-  </Router>
-);
+class App extends React.Component {
+  render() {
+    const { loggedIn } = this.props;
 
-export default App;
+    if (loggedIn) {
+      return (
+        <Router>
+          <div>
+            <Route path="/" exact component={Lobby}/>
+            <Route path="/room/:id" component={PokerRoom}/>
+          </div>
+        </Router>
+      );
+    }
+
+    return (
+      <Router>
+        <div>
+          <Route path="/" exact component={Login}/>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/register" exact component={Register}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default withTracker(() => {
+  return {
+    loggedIn: Meteor.loggingIn() || Meteor.userId() != null,
+  }
+})(App);
